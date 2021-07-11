e74b2fd856b91c0ef84da9c08439ad90eba5c7ad
Masako Gray <masakogray@hotmail.co.uk>
2021-07-11 15:54:39 +0100

    adding active api temparature to interface

--- interface.js
+++ interface.js
@@ -35,12 +35,28 @@ document.addEventListener("DOMContentLoaded", () => {
     updateTemperature();
   })
 
-  fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a2d3431c8c42a066164450a04bbc3074&units=metric')
+  fetch('http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=a2d3431c8c42a066164450a04bbc3074&units=metric') //unit=metric needed to activate
   .then((response) => {
     return response.json()
   })
+  // .then((data) => {
+  //   console.log(data.main.temp); //show visability on console
+  // })
   .then((data) => {
-    console.log(data.main.temp)  
+    document.querySelector('#current-temperature').innerText = data.main.temp;
   });
 
+  const selectElement = document.querySelector('#current-city');
+  selectElement.addEventListener('change', (event) => {
+  const city = event.target.value;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=a2d3431c8c42a066164450a04bbc3074&units=metric`
+
+  fetch(url)
+    .then((response) => {
+      return response.json()
+    })
+    .then((data) => {
+      document.querySelector('#current-temperature').innerText = data.main.temp;
+    })
+  });
 });
